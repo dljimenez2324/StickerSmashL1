4a93dd4788d097e404f87ca4e003bd13953cf4f0
dljimenez2324 <dljimenez2324@gmail.com>
2025-02-10 20:13:11 -0800

    tiny change to file specifially eas.json and json files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -19,7 +19,7 @@ export default function TabsLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          headerTitle: "Sticker Smash",
+          headerTitle: "Sticker Smash Testing",
           tabBarIcon: ({ focused, color }) => (
             <Ionicons name={focused ? "home-sharp" : "home-outline"}
             color={color} 
